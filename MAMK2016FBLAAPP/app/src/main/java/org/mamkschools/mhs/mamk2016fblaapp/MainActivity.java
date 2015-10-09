298d6b50c0ea6c1a4bab9d57627506ff1d89eb09
Andrew Katz <aakatz3@gmail.com>
2015-10-09 09:06:18 -0400

    Commit full app

---
+++ MAMK2016FBLAAPP/app/src/main/java/org/mamkschools/mhs/mamk2016fblaapp/MainActivity.java
@@ -0,0 +1,13 @@
+package org.mamkschools.mhs.mamk2016fblaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MAMK2016FBLAAPP/app/src/test/java/org/mamkschools/mhs/mamk2016fblaapp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package org.mamkschools.mhs.mamk2016fblaapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

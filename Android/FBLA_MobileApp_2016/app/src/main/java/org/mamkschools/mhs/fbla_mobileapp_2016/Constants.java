ed4b34c4e35a5a6c7eabc0ed236e3541acc49f72
Andrew Katz <aakatz3@gmail.com>
2015-10-16 21:17:59 -0400

    Add constants

---
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/Constants.java
@@ -0,0 +1,12 @@
+package org.mamkschools.mhs.fbla_mobileapp_2016;
+
+/**
+ * Constants for APP
+ * Created by Andrew on 10/16/2015.
+ */
+public final class Constants {
+    //API Constants, including URL and valid commands (to be checked before running)
+    public static final String API_BASE_URL = "http://aakatz3.asuscomm.com:8084/fbla2016/api/";
+    public static final String [] API_COMMANDS = {};
+
+}
--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivity.java
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivity.java
@@ -1,13 +1,27 @@
 package org.mamkschools.mhs.fbla_mobileapp_2016;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            default:
+                Toast.makeText(this, "No action implemented", Toast.LENGTH_SHORT).show();
+        }
     }
 }

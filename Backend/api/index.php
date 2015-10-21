4584c4e06da36b7cdf9d5c1f90593d5eef8503a1
Andrew Katz <aakatz3@gmail.com>
2015-10-21 14:26:21 -0400

    Fixed JSON

--- Backend/api/index.php
+++ Backend/api/index.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
-$ret = new array();
-$ret["request"] = _GET['request'];
+$ret = array(
+	"request" => $_GET['request']
+);
 echo json_encode($ret);
 ?>

a8f8f06f469c91a133d4a160e81377628cd1cca9
Andrew Katz <aakatz3@gmail.com>
2015-11-19 11:54:47 -0500

    Fix inittables again

--- Backend/dbscripts/inittables.sql
+++ Backend/dbscripts/inittables.sql
@@ -7,7 +7,7 @@ CREATE TABLE users
 	userid int unsigned not null auto_increment primary key,
 	username char(50) not null,
 	password char(64) not null,
-  salt char(64) not null,
+  salt char(64) not null
 );
 
 CREATE TABLE auth

7d888ca512cfe64ad0e6f72b65bd68aa7b94c08f
Yi Zhao <awesomeyi@gmail.com>
2015-11-21 18:59:41 -0500

    Created custom exception class

---
+++ Backend/api/exception.class.php
@@ -0,0 +1,12 @@
+<?php
+
+	/*
+		Common excpetions used
+		DBConnect: database connect error
+		Auth: authentication error
+	*/
+
+	class DBConnectException extends Exception { }
+	class AuthException extends Exception { }
+
+?>

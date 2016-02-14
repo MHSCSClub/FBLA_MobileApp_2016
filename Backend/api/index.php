5b4279058e34a8078377307ecdeb6d95c2daa408
Andrew Katz <aakatz3@gmail.com>
2016-02-14 12:50:17 -0500

    Fixed bugs with upload detection

--- Backend/api/index.php
+++ Backend/api/index.php
@@ -78,8 +78,8 @@
 		$params = array();
 		$params['geolat']= $_POST['geolat'];
 		$params['geolong'] = $_POST['geolong'];
-		if($_FILES['upfile']['error'] != UPLOAD_ERR_OK) {
-			return Signal::error()->setMessage("File not uploaded")
+		if(!isset($_FILES['picture'])) {
+			return Signal::error()->setMessage("File not uploaded");
 		}
 		$params['picdata'] = file_get_contents($_FILES['picture']['tmp_name']);
 

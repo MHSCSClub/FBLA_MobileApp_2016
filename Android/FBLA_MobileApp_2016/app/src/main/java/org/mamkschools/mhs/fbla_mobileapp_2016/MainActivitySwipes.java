380db35e563ec004255355bac096a425a72115ec
jackphillips7764 <jackphillips@Jacks-MacBook-Pro.local>
2016-02-13 19:19:19 -0500

    Implemented Barebones Activity Swipes

--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
@@ -1,11 +1,16 @@
 package org.mamkschools.mhs.fbla_mobileapp_2016;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Camera;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.MediaStore;
@@ -28,11 +33,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.mamkschools.mhs.fbla_mobileapp_2016.lib.*;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mamkschools.mhs.fbla_mobileapp_2016.PictureContract.*;
 
 public class MainActivitySwipes extends AppCompatActivity implements View.OnClickListener{
 
@@ -69,6 +81,24 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
+        System.out.println("Here");
+        PictureHelper mDbHelper = new PictureHelper(getApplicationContext());
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(PictureEntry.COLUMN_NAME_PICTURE_ID, 1);
+        values.put(PictureEntry.COLUMN_NAME_GEOLAT, 11);
+        values.put(PictureEntry.COLUMN_NAME_GEOLONG, 11);
+        values.put(PictureEntry.COLUMN_NAME_DIST, 1);
+
+        long newRowId;
+        newRowId = db.insert(
+                PictureEntry.TABLE_NAME,
+               "null",
+                values);
+
+
+
 
     }
 
@@ -111,7 +141,10 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
                 break;
             default:
                 Toast.makeText(this, "No Assigned Action", Toast.LENGTH_SHORT).show();
+
         }
+        GetPicture picture = new GetPicture();
+
     }
 
 
@@ -120,35 +153,70 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
+        private Cursor c;
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
+            PictureHelper mDbHelper = new PictureHelper(getApplicationContext());
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+            String[] projection = {
+                    PictureEntry._ID,
+                    PictureEntry.COLUMN_NAME_PICTURE_ID,
+                    PictureEntry.COLUMN_NAME_GEOLONG,
+                    PictureEntry.COLUMN_NAME_GEOLAT
+            };
+
+// How you want the results sorted in the resulting Cursor
+            String sortOrder =
+                    PictureEntry.COLUMN_NAME_PICTURE_ID + " DESC";
+
+            c = db.query(
+                    PictureEntry.TABLE_NAME,  // The table to query
+                    projection,                               // The columns to return
+                    null,                                // The columns for the WHERE clause
+                    null,                            // The values for the WHERE clause
+                    null,                                     // don't group the rows
+                    null,                                     // don't filter by row groups
+                    sortOrder                                 // The sort order
+            );
+            c.moveToFirst();
+            double itemGeoLat = c.getDouble(
+                    c.getColumnIndexOrThrow(PictureEntry.COLUMN_NAME_GEOLAT)
+            );
+            c.moveToLast();
+            System.out.println(itemGeoLat);
+            itemGeoLat = c.getDouble(
+                    c.getColumnIndexOrThrow(PictureEntry.COLUMN_NAME_GEOLAT)
+            );
+            System.out.println(itemGeoLat);
         }
 
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //System.out.println(position);
+            c.moveToPosition(position);
+            int id = c.getInt(
+                    c.getColumnIndexOrThrow(PictureEntry.COLUMN_NAME_PICTURE_ID)
+            );
+            double geolat = c.getDouble(
+                    c.getColumnIndexOrThrow(PictureEntry.COLUMN_NAME_GEOLAT)
+            );
+            return PlaceholderFragment.newInstance(position + 1,id, geolat);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return c.getCount();
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
+            c.moveToPosition(position);
+            String id = c.getString(
+                    c.getColumnIndexOrThrow(PictureEntry.COLUMN_NAME_PICTURE_ID)
+            );
+            return id;
         }
     }
 
@@ -166,15 +234,23 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static PlaceholderFragment newInstance(int sectionNumber, int pictureID, double geolat) {
+            PlaceholderFragment fragment = new PlaceholderFragment(pictureID, geolat);
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
         }
+        private int pictureID;
+        private double geolat;
 
-        public PlaceholderFragment() {
+        public PlaceholderFragment(int pictureID, double geolat) {
+            this.pictureID = pictureID;
+            this.geolat = geolat;
+        }
+        public PlaceholderFragment () {
+            this.pictureID = 0;
+            this.geolat = 0;
         }
 
         @Override
@@ -182,7 +258,7 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main_activity_swipes, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)) + geolat + pictureID);
             return rootView;
         }
     }
@@ -249,6 +325,32 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
                     selectedImageUri = data == null ? null : data.getData();
                 }
             }
+        }
+    }
+    private class GetPicture extends AsyncTask<Void, Void, Void> {
+
+        private ArrayList<JSONObject> ret = new ArrayList<>();
+        SecureAPI picture = SecureAPI.getInstance(MainActivitySwipes.this);
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                JSONObject response = picture.HTTPSGET("picture/fetch?authcode=fd60f0349432ef094b75f864495ee25922d87094dd335610dbdb54f4f88ea48b&geolong=-73.748687&geolat=40.934710&amount=1&ft_dist=100000000000000");
+                JSONArray array = response.getJSONArray("");
+                for(int i = 0; i < array.length(); i ++ ){
+                    System.out.println(array.getJSONObject(i).getJSONObject("data").getInt("pid"));
+                }
+            }catch (Exception e){
+                if(Constants.DEBUG_MODE){
+                    Constants.log(e.getMessage());
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            System.out.println("Cool");
         }
     }
 }

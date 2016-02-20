ed044422ca2220a1f96e7e260ff251eb9a5562f4
Andrew Katz <aakatz3@gmail.com>
2016-02-19 18:06:16 -0600

    Upload fragment works

--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
@@ -132,7 +132,6 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         switch (id){
             case R.id.action_settings:
                 startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
@@ -150,7 +149,7 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
         switch(v.getId()){
             case R.id.fab:
                 //Photo chooser (include default, cam, file (see Google Inbox on Android))
-                openImageIntent();
+                //openImageIntent();
                 break;
             default:
                 Toast.makeText(this, "No Assigned Action", Toast.LENGTH_SHORT).show();
@@ -162,118 +161,10 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
 
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            //System.out.println(position);
-            PictureHelper mDbHelper = new PictureHelper(getApplicationContext());
-            SQLiteDatabase db = mDbHelper.getWritableDatabase();
-            mDbHelper.onUpgrade(db, 1 , 2);
-            int picture = 0;
-            return EvaluationFragment.newInstance(db, picture, location, Simplocation);
-        }
-
-        @Override
-        public int getCount() {
-            return 1;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Section_" + position;
-        }
-    }
-
-    /**
      * A placeholder fragment containing a simple view.
      */
 
-    private Uri outputFileUri;
-    private static final int YOUR_SELECT_PICTURE_REQUEST_CODE = 1;
 
-    private void openImageIntent() {
-        // Determine Uri of camera image to save.
-        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "Pictures" + File.separator);
-        boolean suc = root.mkdirs();
-
-        util.log("makeDirs"+suc);
-        final String fname = "img_"+ System.currentTimeMillis() + ".jpg";
-        final File sdImageMainDirectory = new File(root, fname);
-        outputFileUri = Uri.fromFile(sdImageMainDirectory);
-
-        // Camera.
-        final List<Intent> cameraIntents = new ArrayList<>();
-        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        final PackageManager packageManager = getPackageManager();
-        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
-        for(ResolveInfo res : listCam) {
-            final String packageName = res.activityInfo.packageName;
-            final Intent intent = new Intent(captureIntent);
-            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
-            intent.setPackage(packageName);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-            cameraIntents.add(intent);
-        }
-
-        // Filesystem.
-        final Intent galleryIntent = new Intent();
-        galleryIntent.setType("image/*");
-        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-
-        // Chooser of filesystem options.
-        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
-
-        // Add the camera options.
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
-
-        startActivityForResult(chooserIntent, YOUR_SELECT_PICTURE_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == YOUR_SELECT_PICTURE_REQUEST_CODE) {
-                final boolean isCamera;
-                if (data == null) {
-                    isCamera = true;
-                } else {
-                    final String action = data.getAction();
-                    isCamera = action != null && action.equals(MediaStore.ACTION_IMAGE_CAPTURE);
-                }
-
-                Uri selectedImageUri;
-                if (isCamera) {
-                    selectedImageUri = outputFileUri;
-                } else {
-                    selectedImageUri = data.getData();
-                }
-
-                PicUploadParams uploadPic = new PicUploadParams();
-                uploadPic.pics.put("picture",selectedImageUri);
-                uploadPic.paramMap.put("title", selectedImageUri.toString());
-
-                Constants.LATITUDE = Simplocation.getLatitude();
-                Constants.LONGITUDE = Simplocation.getLongitude();
-
-                util.log("lat" + Constants.LATITUDE + "lon" + Constants.LONGITUDE);
-                //TODO Get lat and long...
-                uploadPic.paramMap.put("geolong", ""+Constants.LONGITUDE);
-                uploadPic.paramMap.put("geolat", ""+Constants.LATITUDE);
-
-                new PicUpload().execute(uploadPic);
-            }
-        }
-    }
 
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
 
@@ -303,44 +194,7 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
         }
     }
 
-    private class PicUpload extends AsyncTask<PicUploadParams, Void, Boolean> {
 
-        private ArrayList<JSONObject> ret = new ArrayList<>();
-        SecureAPI picture = SecureAPI.getInstance(MainActivitySwipes.this);
-
-        @Override
-        protected Boolean doInBackground(PicUploadParams... params) {
-
-            try {
-                JSONObject response = picture.HTTPSPOSTMULTI(Commands.Post.POSTPIC +
-                        Commands.AUTHCODE_BASE + Constants.AUTHCODE,
-                        params[0].paramMap, params[0].pics, getApplicationContext());
-                if(response.getString("status").equals("success")){
-                    return true;
-                } else if(response.getString("status").equals("error")){
-                    return false;
-                } else {
-                    throw new Exception("Impossible status");
-                }
-            }catch (Exception e){
-                e.printStackTrace();
-                if(Constants.DEBUG_MODE){
-                    util.log(e.getMessage());
-
-                }
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            if(success){
-                util.log("Upload worked");
-            } else {
-                util.log("Upload failed");
-            }
-        }
-    }
     private class Logout extends AsyncTask<Void, Void, Void>{
         @Override
         protected Void doInBackground(Void ... v) {
@@ -362,8 +216,5 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
         }
     }
 
-    private static class PicUploadParams{
-        public Map<String, String> paramMap = new HashMap<String, String>();
-        public Map<String, Uri> pics = new HashMap<String, Uri>();
-    }
+
 }

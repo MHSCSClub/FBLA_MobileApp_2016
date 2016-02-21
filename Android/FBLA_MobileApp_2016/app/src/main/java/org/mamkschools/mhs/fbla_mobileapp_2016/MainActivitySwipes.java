2eb91a785c8d9e78a6908ea70cbeadd63f6b6b25
Andrew Katz <aakatz3@gmail.com>
2016-02-20 19:20:58 -0500

    code cleanup

--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/MainActivitySwipes.java
@@ -1,54 +1,31 @@
 package org.mamkschools.mhs.fbla_mobileapp_2016;
 
-import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.database.sqlite.SQLiteDatabase;
-
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
-import android.os.Parcelable;
-import android.provider.MediaStore;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.mamkschools.mhs.fbla_mobileapp_2016.lib.*;
+import org.mamkschools.mhs.fbla_mobileapp_2016.lib.Commands;
+import org.mamkschools.mhs.fbla_mobileapp_2016.lib.Constants;
+import org.mamkschools.mhs.fbla_mobileapp_2016.lib.PictureHelper;
+import org.mamkschools.mhs.fbla_mobileapp_2016.lib.SecureAPI;
+import org.mamkschools.mhs.fbla_mobileapp_2016.lib.util;
 
 import java.io.File;
-import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import im.delight.android.location.SimpleLocation;
-
-import static org.mamkschools.mhs.fbla_mobileapp_2016.lib.PictureContract.*;
 
 public class MainActivitySwipes extends AppCompatActivity implements View.OnClickListener {
 
@@ -78,7 +55,7 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
             SimpleLocation.openSettings(this);
         }
 
-        if(!Constants.PREFS_RESTORED){
+        if (!Constants.PREFS_RESTORED) {
             Constants.restorePrefs(getApplicationContext());
         }
 
@@ -111,19 +88,17 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
     }
 
 
-
-
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        if(Constants.AUTHCODE == null){
+        if (Constants.AUTHCODE == null) {
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
         Constants.restorePrefs(getApplicationContext());
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Constants.PREFS_RESTORED = false;
         Constants.savePrefs(getApplicationContext());
@@ -144,7 +119,7 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch (id){
+        switch (id) {
             case R.id.action_logout:
                 new Logout().execute((Void) null);
                 return true;
@@ -155,13 +130,14 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.fab:
                 //Photo chooser (include default, cam, file (see Google Inbox on Android))
                 //openImageIntent();
                 break;
             case R.id.more_stuff:
-                startActivity(new Intent(getApplicationContext(), null));
+                new Logout().execute((Void) null);
+                //startActivity(new Intent(getApplicationContext(), null));
                 break;
             default:
                 Toast.makeText(this, "No Assigned Action", Toast.LENGTH_SHORT).show();
@@ -177,7 +153,6 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
      */
 
 
-
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
 
         private String[] titles = {"Evaluate", "Me"};
@@ -188,10 +163,13 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
 
         @Override
         public Fragment getItem(int pos) {
-            switch(pos) {
-                case 0: return evaluatePictures;
-                case 1: return myPictures;
-                default: return evaluatePictures;
+            switch (pos) {
+                case 0:
+                    return evaluatePictures;
+                case 1:
+                    return myPictures;
+                default:
+                    return evaluatePictures;
             }
         }
 
@@ -207,9 +185,9 @@ public class MainActivitySwipes extends AppCompatActivity implements View.OnClic
     }
 
 
-    private class Logout extends AsyncTask<Void, Void, Void>{
+    private class Logout extends AsyncTask<Void, Void, Void> {
         @Override
-        protected Void doInBackground(Void ... v) {
+        protected Void doInBackground(Void... v) {
             SecureAPI HTTPS = SecureAPI.getInstance(getApplicationContext());
 
             try {

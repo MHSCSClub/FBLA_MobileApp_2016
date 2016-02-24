ec6cba7e5e189e852a2d50343b08822f06056fba
Yi Zhao <awesomeyi@gmail.com>
2016-02-24 12:53:22 -0500

    Added detailme and improved comments

---
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/DetailMe.java
@@ -0,0 +1,29 @@
+package org.mamkschools.mhs.fbla_mobileapp_2016;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_me);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

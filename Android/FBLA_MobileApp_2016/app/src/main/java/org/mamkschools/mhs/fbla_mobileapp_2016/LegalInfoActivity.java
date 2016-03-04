28145f4dff19e463792ec156e7ef620372a514ea
Andrew Katz <aakatz3@gmail.com>
2016-03-04 11:40:36 -0500

    Fixed a super minor newline bug

--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/LegalInfoActivity.java
+++ Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/LegalInfoActivity.java
@@ -20,7 +20,7 @@ public class LegalInfoActivity extends AppCompatActivity {
         String text = "";
         try{
             do{
-                text += s.nextLine();
+                text += s.nextLine() + "\n";
             } while(s.hasNextLine());
         }catch(Exception e){
             Debug.log(e.getMessage());

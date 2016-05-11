0ab0f49a9584f67d6baa068d0a614c755370c9fd
Andrew Katz <aakatz3@gmail.com>
2016-05-10 21:11:16 -0400

    removed useless interface

--- Android/FBLA_MobileApp_2016/app/src/main/java/org/mamkschools/mhs/fbla_mobileapp_2016/lib/FragmentVisibilityInterface.java
+++
@@ -1,8 +0,0 @@
-package org.mamkschools.mhs.fbla_mobileapp_2016.lib;
-
-/**
- * Created by aakat on 5/10/2016.
- */
-public interface FragmentVisibilityInterface {
-    void onFragmentBecameVisible();
-}
